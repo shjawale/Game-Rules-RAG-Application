ca11822f48778af4b57a798d70f758ad2da025c5
shjawale <shruti.s.jawale@gmail.com>
2026-02-28 10:19:31 -0800

    added necessary files

--- gen-ai-intensive-course-capstone-2025.py
+++ gen-ai-intensive-course-capstone-2025.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import seaborn as sns
 import os
 from dotenv import load_dotenv
 
+
 from google import genai
 from google.genai import types
 import chromadb
 import functools
 
+
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 HOME_DIR = os.getenv("HOME")
+
 
 # Load .env with explicit path
 env_path = os.path.join(CURRENT_DIR, ".env")
 load_dotenv(dotenv_path=env_path, override=True)
 
+
 # Set up API key
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 
 # Create a RAG application to help you understand the rules of any board game
 #  Explore available models
@@ -28,17 +34,22 @@ client = genai.Client(api_key=GOOGLE_API_KEY)
 
 for m in client.models.list():
     if "embedContent" in m.supported_actions:
-        
+        print(m.name)
+
 
 #  Add data
 import kagglehub
-dataset_path = kagglehub.dataset_download("sujaykapadnis/board-games") 
+dataset_path = kagglehub.dataset_download("sujaykapadnis/board-games") #/versions/1/board_games.csv")
+print("Path to dataset files:", dataset_path)
 
 DATASET_PATH = os.path.join(dataset_path, "board_games.csv")
 
+
 df = pd.read_csv(DATASET_PATH)
 
+print(df["image"][21:23])
 df.head()
+
 
 #  Convert data df to list of documents with each document as the name of game + descriptions
 #start with creating a single document.
@@ -46,6 +57,7 @@ n = len(df)
 maxlen = 100
 start = 0
 documents = [str(df["game_id"][i]) + " " + str(df["name"][i]) + ": " + str(df["description"][i]) for i in range(start, start+maxlen)]
+
 
 # Creating the embedding database with ChromaDB
 
@@ -76,6 +88,7 @@ class GeminiEmbeddingFunction(EmbeddingFunction):
         )
         return [e.values for e in response.embeddings]
 
+
 #  create a Chroma database client
 
 DB_NAME = "boardgamedatabase"
@@ -88,6 +101,7 @@ db = chroma_client.get_or_create_collection(name=DB_NAME, embedding_function=emb
 
 db.add(documents=documents, ids=[str(i) for i in range(len(documents))])
 
+
 # Retrieval: Find relevant documents¶
 
 # Switch to query mode when generating embeddings.
@@ -98,6 +112,9 @@ query = "What's the first step in Twilight Imperium?\nHow do you play Twilight I
 
 result = db.query(query_texts=[query], n_results=2)
 [all_passages] = result["documents"]
+
+print(all_passages[0])
+
 
 # Augmented generation: Answer the question
 
@@ -113,10 +130,16 @@ for passage in all_passages:
     passage_oneline = passage.replace("\n", " ")
     prompt += f"PASSAGE: {passage_oneline}\n"
 
+print(prompt)
+
+
 AG_all_qs_answer = client.models.generate_content(
     model="gemini-2.0-flash",
     contents=prompt
 )
+
+print(AG_all_qs_answer.text)
+
 
 # Evaluation and structured output
 
@@ -143,6 +166,7 @@ questions = [
 if not questions:
   raise NotImplementedError('Add some questions to evaluate!')
 
+
 @functools.cache
 def answer_question(questions: tuple, guidance: str = '') -> str:
   """Generate an answer to the question using the uploaded document and guidance."""
@@ -157,6 +181,7 @@ def answer_question(questions: tuple, guidance: str = '') -> str:
   )
 
   return response.text
+
 
 import enum
 
@@ -205,6 +230,7 @@ class AnswerRating(enum.Enum):
   BAD = '2'
   WRONG = '1'
 
+
 @functools.cache
 def eval_answer(prompt, ai_response, n=1):
   """Evaluate the generated answer against the prompt/question used."""
@@ -221,7 +247,7 @@ def eval_answer(prompt, ai_response, n=1):
       response_mime_type="text/x.enum",
       response_schema=AnswerRating,
   )
-  
+  print('\n\n')
   
   response = chat.send_message(
       message="Convert the final score.",
@@ -231,8 +257,12 @@ def eval_answer(prompt, ai_response, n=1):
   
   return verbose_eval, structured_eval
 
+print(AG_all_qs_answer.text)
 text_eval, struct_eval = eval_answer(prompt=tuple(questions), ai_response=tuple(AG_all_qs_answer.text))
-display(
+display(print(text_eval))
+print()
+print(struct_eval)
+
 
 # Google Search Grounding
 
@@ -243,6 +273,9 @@ response = client.models.generate_content(
     model='gemini-2.0-flash',
     contents=questions
 )
+
+print(response.text)
+
 
 # And now re-run the same query with search grounding enabled.
 config_with_search = types.GenerateContentConfig(
@@ -257,7 +290,10 @@ def query_with_grounding():
     )
     return grounding_response.candidates[0]
 
+
 rc = query_with_grounding()
+print(rc.content.parts[0].text)
+
 
 while not rc.grounding_metadata.grounding_supports or not rc.grounding_metadata.grounding_chunks:
     # If incomplete grounding data was returned, retry.
@@ -265,15 +301,18 @@ while not rc.grounding_metadata.grounding_supports or not rc.grounding_metadata.
 
 chunks = rc.grounding_metadata.grounding_chunks
 for chunk in chunks:
-    
+    print(f'{chunk.web.title}: {chunk.web.uri}')
 
-HTML(rc.grounding_metadata.search_entry_point.rendered_content)
 
-from p
+print(rc.grounding_metadata.search_entry_point.rendered_content)
+
+
+from pprint import pprint
 
 supports = rc.grounding_metadata.grounding_supports
 for support in supports:
-    
+    print(support.to_json_dict())
+
 
 import io
 
@@ -293,9 +332,13 @@ for support in supports:
 
     markdown_buffer.write("\n\n")
 
+
 markdown_buffer.write("Citations:\n\n")
 for i, chunk in enumerate(chunks, start=1):
     markdown_buffer.write(f"{i}. [{chunk.web.title}]({chunk.web.uri})\n")
+
+print(markdown_buffer.getvalue())
+
 
 # Embeddings and similarity scores
 
@@ -304,13 +347,14 @@ for i, chunk in enumerate(chunks, start=1):
 def show_response(response):
     for p in response.candidates[0].content.parts:
         if p.text:
-            display(
+            display(print(p.text))
         elif p.inline_data:
             display(Image(p.inline_data.data))
         else:
-            
+            print(p.to_json_dict())
     
-        display(
+        display(print('----'))
+
 
 config_with_search = types.GenerateContentConfig(
     tools=[types.Tool(google_search=types.GoogleSearch())],
@@ -326,6 +370,10 @@ response = chat.send_message(
 
 show_response(response)
 
+
+print(response.text)
+
+
 config_with_code = types.GenerateContentConfig(
     tools=[types.Tool(code_execution=types.ToolCodeExecution())],
     temperature=0.0,
@@ -338,15 +386,21 @@ response = chat.send_message(
 
 show_response(response)
 
+
 ## Calculate similarity scores
 
 texts = [AG_all_qs_answer.text,
          rc.content.parts[0].text]
 
+print(AG_all_qs_answer.text)
+print('\n\n')
+print(rc.content.parts[0].text)
+
 response = client.models.embed_content(
     model='models/text-embedding-004',
     contents=texts,
     config=types.EmbedContentConfig(task_type='semantic_similarity'))
+
 
 def truncate(t: str, limit: int = 40) -> str:
   """Truncate labels to fit on the chart."""
@@ -357,6 +411,7 @@ def truncate(t: str, limit: int = 40) -> str:
 
 truncated_texts = [truncate(t) for t in texts]
 
+
 # Set up the embeddings in a dataframe.
 df = pd.DataFrame([e.values for e in response.embeddings], index=truncated_texts)
 # Perform the similarity calculation
@@ -364,4 +419,7 @@ sim = df @ df.T
 # Draw!
 sns.heatmap(sim, vmin=0, vmax=1, cmap="Blues");
 
+
+print()
+print(sim)
 
